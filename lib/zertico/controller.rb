7667989bf6e846e767680cf57faffb5ec3aa707d
Paulo Henrique Lopes Ribeiro <plribeiro3000@gmail.com>
2013-09-04 10:33:40 -0300

    Improving Controller and Changing api to favour responder customization

--- lib/zertico/controller.rb
+++ lib/zertico/controller.rb
@@ -13,54 +13,43 @@ module Zertico
 
     def index
       initialize_object all
-      respond_with(instance_variable_get('@responder'))
+      respond_with(@responder, @options)
     end
 
     def new
       initialize_object build
-      respond_with(instance_variable_get('@responder'))
+      respond_with(@responder, @options)
     end
 
     def show
       initialize_object find(params[interface_id.to_sym])
-      respond_with(instance_variable_get('@responder'))
+      respond_with(@responder, @options)
     end
 
     def edit
       initialize_object find(params[interface_id.to_sym])
-      respond_with(instance_variable_get('@responder'))
+      respond_with(@responder, @options)
     end
 
     def create
       initialize_object generate(params[interface_name.to_sym])
-      if instance_variable_defined?('@location')
-        respond_with(instance_variable_get('@responder'), :location => instance_variable_get('@location'))
-      else
-        respond_with(instance_variable_get('@responder'))
-      end
+      respond_with(@responder, @options)
     end
 
     def update
       initialize_object modify(params[interface_id.to_sym], params[interface_name.to_sym])
-      if instance_variable_defined?('@location')
-        respond_with(instance_variable_get('@responder'), :location => instance_variable_get('@location'))
-      else
-        respond_with(instance_variable_get('@responder'))
-      end
+      respond_with(@responder, @options)
     end
 
     def destroy
       initialize_object delete(params[interface_id.to_sym])
-      if instance_variable_defined?('@location')
-        respond_with(instance_variable_get('@responder'), :location => instance_variable_get('@location'))
-      else
-        respond_with(instance_variable_get('@responder'))
-      end
+      respond_with(@responder, @options)
     end
 
     protected
 
-    def initialize_object(objects = {})
+    def initialize_object(objects = {}, options = {})
+      @options = options
       objects.each do |key, value|
         instance_variable_set("@#{key}", value)
         instance_variable_set('@responder', value) unless @responder
