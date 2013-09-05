bb3b7fdc10a430a7b2e7ac66322a1541f558d57e
Paulo Henrique Lopes Ribeiro <plribeiro3000@gmail.com>
2013-09-05 19:37:40 -0300

    Adding module to add in responder to force redirect if needed

--- lib/zertico.rb
+++ lib/zertico.rb
@@ -5,4 +5,5 @@ module Zertico
   autoload :Controller, 'zertico/controller'
   autoload :Service, 'zertico/service'
   autoload :Accessor, 'zertico/accessor'
+  autoload :Responder, 'zertico/responder'
 end
