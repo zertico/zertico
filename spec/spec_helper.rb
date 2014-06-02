671b0eb4f8cc2432d14b57db3e1cb3308359ad3a
Paulo Henrique Lopes Ribeiro <plribeiro3000@gmail.com>
2014-06-02 14:26:17 -0300

    Ignoring fake_app folder on code coverage generation

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,11 @@
 require 'rspec'
+require 'simplecov'
 require 'coveralls'
+
+SimpleCov.formatter = Coveralls::SimpleCov::Formatter
+SimpleCov.start do
+  add_filter "#{File.dirname(__FILE__)}/fake_app"
+end
 
 Coveralls.wear!
 
