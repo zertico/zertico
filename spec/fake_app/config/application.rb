d8fdfe16c96039fe30eb97ab7e1b2430ff7f46b6
Paulo Henrique Lopes Ribeiro <plribeiro3000@gmail.com>
2014-06-10 00:25:26 -0300

    Fixing build on rails 4

--- spec/fake_app/config/application.rb
+++ spec/fake_app/config/application.rb
@@ -5,5 +5,6 @@ module RailsApplication
   class Application < Rails::Application
     config.root = "#{config.root}/spec/fake_app"
     config.logger = nil
+    config.secret_key_base = YAML.load(File.open("#{Rails.root}/config/secrets.yml"))[Rails.env]['secret_key_base']
   end
 end
