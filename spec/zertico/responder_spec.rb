9be1eaefb00f2f3633e1aeb59ce5359e2614e61f
Paulo Henrique Lopes Ribeiro <plribeiro3000@gmail.com>
2014-07-02 11:04:16 -0300

    Testing Pjax behavior on responder

--- spec/zertico/responder_spec.rb
+++ spec/zertico/responder_spec.rb
@@ -10,58 +10,126 @@ describe UsersResponder, :type => :controller do
   render_views
 
   describe '#index' do
-    before :each do
-      get :index
+    context 'from normal requisition' do
+      before :each do
+        get :index
+      end
+
+      it 'should have an HTTP status :ok' do
+        expect(response).to have_http_status(:ok)
+      end
+
+      it 'should render template index' do
+        expect(response).to render_template(:index)
+      end
+
+      it 'should render the application layout' do
+        expect(response).to render_template(:layout => 'application')
+      end
     end
 
-    it 'should have an HTTP status :ok' do
-      expect(response).to have_http_status(:ok)
-    end
+    context 'from pjax' do
+      before :each do
+        request.headers['X-PJAX'] = 'true'
+        get :index
+      end
 
-    it 'should render template index' do
-      expect(response).to render_template(:index)
+      it 'should not render any layout' do
+        expect(response).to render_template(:layout => false)
+      end
     end
   end
 
   describe '#new' do
-    before :each do
-      get :show, :id => 1
+    context 'from normal requisition' do
+      before :each do
+        get :new
+      end
+
+      it 'should have an HTTP status :ok' do
+        expect(response).to have_http_status(:ok)
+      end
+
+      it 'should render template new' do
+        expect(response).to render_template(:new)
+      end
+
+      it 'should render the application layout' do
+        expect(response).to render_template(:layout => 'application')
+      end
     end
 
-    it 'should have an HTTP status :ok' do
-      expect(response).to have_http_status(:ok)
-    end
+    context 'from pjax' do
+      before :each do
+        request.headers['X-PJAX'] = 'true'
+        get :new
+      end
 
-    it 'should render template new' do
-      expect(response).to render_template(:show)
+      it 'should not render any layout' do
+        expect(response).to render_template(:layout => false)
+      end
     end
   end
 
   describe '#show' do
-    before :each do
-      get :show, :id => 1
+    context 'from normal requisition' do
+      before :each do
+        get :show, :id => 1
+      end
+
+      it 'should have an HTTP status :ok' do
+        expect(response).to have_http_status(:ok)
+      end
+
+      it 'should render template show' do
+        expect(response).to render_template(:show)
+      end
+
+      it 'should render the application layout' do
+        expect(response).to render_template(:layout => 'application')
+      end
     end
 
-    it 'should have an HTTP status :ok' do
-      expect(response).to have_http_status(:ok)
-    end
+    context 'from pjax' do
+      before :each do
+        request.headers['X-PJAX'] = 'true'
+        get :show, :id => 1
+      end
 
-    it 'should render template show' do
-      expect(response).to render_template(:show)
+      it 'should not render any layout' do
+        expect(response).to render_template(:layout => false)
+      end
     end
   end
 
   describe '#edit' do
-    before :each do
-      get :edit, :id => 1
+    context 'from normal requisition' do
+      before :each do
+        get :edit, :id => 1
+      end
+
+      it 'should have an HTTP status :ok' do
+        expect(response).to have_http_status(:ok)
+      end
+
+      it 'should render template edit' do
+        expect(response).to render_template(:edit)
+      end
+
+      it 'should render the application layout' do
+        expect(response).to render_template(:layout => 'application')
+      end
     end
 
-    it 'should have an HTTP status :ok' do
-      expect(response).to have_http_status(:ok)
-    end
+    context 'from pjax' do
+      before :each do
+        request.headers['X-PJAX'] = 'true'
+        get :edit, :id => 1
+      end
 
-    it 'should render template edit' do
-      expect(response).to render_template(:edit)
+      it 'should not render any layout' do
+        expect(response).to render_template(:layout => false)
+      end
     end
   end
 
@@ -115,7 +183,6 @@ describe UsersResponder, :type => :controller do
     context 'when fail to save' do
       before :each do
         User.stub(:find => user)
-        user.stub(:update_attributes => false)
         user.stub(:errors => ['asd'])
         put :update, :id => 1
       end
